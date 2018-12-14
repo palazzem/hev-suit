f2d8402f5fd13a4a96750683865d7335e178a550
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2018-12-14 22:20:41 +0100

    [core] right exports in __init__

--- hev/__init__.py
+++ hev/__init__.py
@@ -1,11 +1,13 @@
-from . import exceptions
+from . import auth
 
+from .api import DatadogAPI
 from .config import Config
 
 
 conf = Config()
 
 __all__ = [
+    "auth",
     "conf",
-    "exceptions",
+    "DatadogAPI",
 ]
