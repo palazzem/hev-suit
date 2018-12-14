5c413fada854fe7eeb298dd407f567786be75d33
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2018-12-14 22:20:00 +0100

    [core] add authorization layer

---
+++ hev/auth.py
@@ -0,0 +1,31 @@
+from .exceptions import NotAuthorized
+
+
+def is_authorized(request, authorized_token):
+    """Return if the given request is authorized.
+
+    A request is considered authorized to access the functionalities
+    offered by HEV suit if it includes a Bearer token in transmitted
+    headers.
+
+    Expected format is `Authorization: Bearer <TOKEN>`.
+
+    Args:
+        request: A Flask Request object
+        authorized_token: A Bearer token considered valid
+
+    Returns:
+        A boolean where ``True`` means the request is authorized
+
+    Raises:
+        NotAuthorized: An error occurred when the request is not
+            authorized. It's raised when a Bearer token is missed
+            or is wrong.
+    """
+    auth_header = request.headers.get("Authorization")
+
+    # do something only if request contains a Bearer token
+    if auth_header is None or not auth_header.startwith("Bearer"):
+        raise NotAuthorized("Authorization headers are missing")
+
+    return auth_header[7:] == authorized_token
