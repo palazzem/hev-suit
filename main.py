2f0c233b7b320eed20d80ef0ef3161185b7b5728
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2018-12-14 22:21:27 +0100

    [core] update webhook entrypoint to use the new design

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import logging
 
-from hev import config
+from hev import conf, DatadogAPI, auth
+from hev.constants import KIND_DIASTOLIC, KIND_SYSTOLIC
+from hev.exceptions import ConfigException, NotAuthorized
 
 
-def main():
-    """Cloud Function entrypoint."""
+def webhook(request):
+    """Cloud Function entrypoint.
+
+    Args:
+        request: Flask Request object
+
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    # Validate Environment Configuration
     try:
-        api = config.init()
-    except RuntimeError as e:
+        conf.validate()
+        auth.is_authorized(request, conf.bearer_token)
+    except ConfigException as e:
         logging.critical("Unable to configure Cloud Function: %s", str(e))
         return
+    except NotAuthorized as e:
+        # TODO: update the response so that it can be parsed from a
+        # Google Action
+        logging.critical(str(e))
+        return ("Not Authorized", 401)
+
+    # Prepare the API
+    api = DatadogAPI(conf.dd_api_key, conf.function_name)
 
     # Collect values from Cloud Datastore
-    # TODO: NotImplemented
+    # TODO: Static values, NotImplemented
     bpm, value_min, value_max = 70, 80, 144
 
-    # Send values to Datadog
+    # Send HEV parameters to Datadog
+    # TODO: check responses and return the right answer to the client
     api.send_bpm(bpm)
-    api.send_pressure(value_min, value_max)
+    api.send_pressure(value_min, kind=KIND_DIASTOLIC)
+    api.send_pressure(value_max, kind=KIND_SYSTOLIC)
 
     logging.info("Cloud Function executed correctly.")
